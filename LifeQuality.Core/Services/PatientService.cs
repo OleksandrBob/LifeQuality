c71a69bfffcfff11cf3863951adbb6fe51184591
ViktoriaTomchyshyn <viktoriia.tomchyshyn@gmail.com>
2023-12-04 14:14:48 +0300

    Patient cabinet endpoints

--- LifeQuality.Core/Services/PatientService.cs
+++ LifeQuality.Core/Services/PatientService.cs
@@ -1,3 +1,4 @@
+using CSharpFunctionalExtensions;
 using LifeQuality.Core.Services.Interfaces;
 using LifeQuality.DAL.Context;
 using LifeQuality.DAL.Model;
@@ -32,4 +33,25 @@ public class PatientService : IPatientService
 
         return result;
     }
+
+    public async Task<List<Analysis>> GetPatientAnalysis(int patientId)
+    {
+        var result = _dataContext.Analyses.Where(p =>
+            p.PatientId == patientId);
+
+        _logger.LogInformation($"Gotten count - {result.Count()} analysis for doctor {patientId}");
+
+        return await result.ToListAsync();
+    }
+
+    public async Task<Doctor> GetPatientDoctor(int patientId)
+    {
+        var patient = await _dataContext.Patients.FirstAsync(p => p.Id == patientId);
+        var result = await _dataContext.Doctors.FirstAsync(d => d.Id == patient.DoctorId);
+
+        _logger.LogInformation($"Gotten id {result.Id} doctor for patient {patientId}");
+
+        return result;
+    }
+
 }
---
+++ LifeQuality.Server/Comands/Patient/GetPatientDoctorQuery.cs
@@ -0,0 +1,43 @@
+﻿using CSharpFunctionalExtensions;
+using LifeQuality.Core.Dto;
+using LifeQuality.Core.Services.Interfaces;
+using LifeQuality.DAL.Model;
+using MediatR;
+
+namespace LifeQuality.Server.Comands.Patient;
+
+public class GetPatientDoctorQuery : IRequest<Result<Doctor>>
+{
+    public int PatientId { get; set; }
+
+    public class Handler : IRequestHandler<GetPatientDoctorQuery, Result<Doctor>>
+    {
+        private readonly IPatientService _patientService;
+
+        public Handler(IPatientService patientService)
+        {
+            _patientService = patientService;
+        }
+
+        public async Task<Result<Doctor>> Handle(GetPatientDoctorQuery request,
+            CancellationToken cancellationToken)
+        {
+            var doctor = await _patientService.GetPatientDoctor(request.PatientId);
+
+            if (doctor == null)
+            {
+                return Result.Failure<Doctor>("Doctor not fount");
+            }
+
+            var result = new Doctor
+            {
+                Id = doctor.Id,
+                Name = doctor.Name,
+                Email = doctor.Email,
+                Surname = doctor.Surname
+            };
+
+            return Result.Success<Doctor>(result);
+        }
+    }
+}
